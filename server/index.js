694d18343bdc0a38dcde7264a1ed0c14852882c5
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-08-02 01:17:49 +0300

    register router

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const authRouter = require("./routes/auth");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
