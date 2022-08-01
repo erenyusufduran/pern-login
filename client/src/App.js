ab6de8dbe088baf25bdeffb35080fac49031a515
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-08-02 02:18:05 +0300

    setting up react and react-router-dom

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { Fragment, useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            ></Route>
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            ></Route>
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            ></Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const Login = ({ setAuth }) => {
+  return (
+    <Fragment>
+      <h1>Login</h1>
+      <button onClick={() => setAuth(true)}>Login</button>
+    </Fragment>
+  );
+};
+
+export default Login;
